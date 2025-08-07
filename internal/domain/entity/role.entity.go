703c02d388cad338e7fa854d228d032efeb5468b
owlprojection <bagus.yanuar613@gmail.com>
2025-08-07 14:13:40 +0700

    feat: create entity role & permissions

---
+++ internal/domain/entity/role.entity.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Role struct {
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Permissions []Permission `gorm:"many2many:role_permissions;constraint:OnDelete:CASCADE;"`
+}
+
+func (r *Role) BeforeCreate(tx *gorm.DB) (err error) {
+	if r.ID == uuid.Nil {
+		r.ID = uuid.New()
+	}
+	r.CreatedAt = time.Now()
+	r.UpdatedAt = time.Now()
+	return
+}
+
+func (r *Role) TableName() string {
+	return "roles"
+}
